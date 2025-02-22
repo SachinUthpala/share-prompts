72278ce145c55e7a37c774d1f9ed77d520503010
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2025-02-22 22:41:33 +0530

    some main changes

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,17 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Discover and Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">AI Powered Prompts</span>
+        </h1>
+
+        <p className="desc text-center">
+            Pormtopia is a ai based prompt shareing and discovering tool for easy to share and discover you needs
+        </p>
+
+    </section>
   )
 }
 
